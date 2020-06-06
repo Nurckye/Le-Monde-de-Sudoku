c4453d61a8eaca4a64d3f6c033f705ae1de1097e
Nurckye <radu.nitescu35@gmail.com>
2020-06-06 18:58:59 +0300

    Updates

---
+++ precache-manifest.5b56c9c0cb93401b621f32a3ebd12927.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be4306b0cb04202570c4be0b2ae8ed3",
+    "url": "/Le-Monde-de-Sudoku/index.html"
+  },
+  {
+    "revision": "e0e4cc57fb86609d2dba",
+    "url": "/Le-Monde-de-Sudoku/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "25723adc1f8227cb8a9b",
+    "url": "/Le-Monde-de-Sudoku/static/css/main.a7651c70.chunk.css"
+  },
+  {
+    "revision": "e0e4cc57fb86609d2dba",
+    "url": "/Le-Monde-de-Sudoku/static/js/2.1fe75f82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Le-Monde-de-Sudoku/static/js/2.1fe75f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25723adc1f8227cb8a9b",
+    "url": "/Le-Monde-de-Sudoku/static/js/main.a3dbf04f.chunk.js"
+  },
+  {
+    "revision": "2c1bfd2d575497506973",
+    "url": "/Le-Monde-de-Sudoku/static/js/runtime-main.cd05e16b.js"
+  },
+  {
+    "revision": "ab340eb20d8bd0911acd42a237becaec",
+    "url": "/Le-Monde-de-Sudoku/static/media/click1.ab340eb2.mp3"
+  },
+  {
+    "revision": "fe6563e94933145f54565923ad4120c5",
+    "url": "/Le-Monde-de-Sudoku/static/media/click2.fe6563e9.mp3"
+  },
+  {
+    "revision": "1f3e0cf99c31a4035a5ec2024095e999",
+    "url": "/Le-Monde-de-Sudoku/static/media/github.1f3e0cf9.png"
+  },
+  {
+    "revision": "7ce585cd4382e1b9c6193cc93eb666fe",
+    "url": "/Le-Monde-de-Sudoku/static/media/instagram.7ce585cd.png"
+  },
+  {
+    "revision": "388b83674f3ed854792e15e335343996",
+    "url": "/Le-Monde-de-Sudoku/static/media/linkedin.388b8367.png"
+  },
+  {
+    "revision": "ee51dfb4d5ddf692360b42229941ec50",
+    "url": "/Le-Monde-de-Sudoku/static/media/logo512.ee51dfb4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.16dc049eb053e989942b9f71f5f46d35.js"
+  "/Le-Monde-de-Sudoku/precache-manifest.5b56c9c0cb93401b621f32a3ebd12927.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Le-Monde-de-Sudoku/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
